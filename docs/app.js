c656d016d94f7cbf887885bf6dad87aabfe1faad
SurlyKM <142463016+SurlyKM@users.noreply.github.com>
2026-08-23 11:39:40 +1000

    update

--- docs/app.js
+++ docs/app.js
@@ -446,8 +446,8 @@
     let location = '';
     if (ex.address) location = ex.address;
     if (ex.suburb) location += (location ? ', ' : '') + ex.suburb;
-    return 'https://calendar.google.com/calendar/r/eventedit'
-      + '?text=' + encodeURIComponent(title)
+    return 'https://www.google.com/calendar/render?action=TEMPLATE'
+      + '&text=' + encodeURIComponent(title)
       + '&dates=' + dates
       + '&ctz=Australia/Sydney'
       + '&details=' + encodeURIComponent(ex.description || '')
